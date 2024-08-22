784c8395a7d3d313c039dfcdca6f38bd4820094a
thorbjornsen98 <august.thorbjornsen@hotmail.com>
2024-08-22 14:22:25 +0200

    first commit

---
+++ DAT101-main/common/script/utils.mjs
@@ -0,0 +1,17 @@
+"use strict";
+
+let DOMTextOut = null;
+export const newLine = "<br/>";
+export const NEWLine = newLine;
+
+export function initPrintOut(aDomElement) {
+  DOMTextOut = aDomElement;
+}
+
+export function printOut(aText) {
+  aText = aText.replace(newLine, "");
+  if (DOMTextOut.innerHTML.length !== 0) {
+    DOMTextOut.innerHTML += newLine;
+  }
+  DOMTextOut.innerHTML += aText;
+}
