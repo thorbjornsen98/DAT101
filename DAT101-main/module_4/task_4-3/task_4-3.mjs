c959787648f097b7fb51f226f3fad5901eb12d3e
thorbjornsen98 <august.thorbjornsen@hotmail.com>
2024-12-26 21:43:33 +0100

    4.3 begynelse

--- DAT101-main/module_4/task_4-3/task_4-3.mjs
+++ DAT101-main/module_4/task_4-3/task_4-3.mjs
@@ -1,5 +1,83 @@
 "use strict";
 
+import { newLine } from "../../common/script/utils.mjs";
+
+//--- Part 1 ----------------------------------------------------------------------------------------------
+/* Put your code below here!*/
+
+const cmbTask1Calculate = document.getElementById("cmbTask1Calculate");
+cmbTask1Calculate.addEventListener("click", cmbTask1CalculateClick);
+
+function cmbTask1CalculateClick() {
+
+ const txtRectHeight = document.getElementById("txtRectHeight");
+  const txtRectWidth = document.getElementById("txtRectWidth");
+  const height = Number(txtRectHeight.value);
+  const width = Number(txtRectWidth.value);
+
+  
+  const perimeter = (height + width + height + width);
+  const area = (height * width);
+  const txtTask1Output = document.getElementById("txtTask1Output");
+  txtTask1Output.innerHTML = "Omkrets: " + perimeter + "&nbsp; Areal: " + area;
+}
+
+//--- Part 2 ----------------------------------------------------------------------------------------------
+/* Put your code below here!*/
+
+const txtTask2Word = document.getElementById("txtTask2Word");
+txtTask2Word.addEventListener("keypress", txtTask2WordKeyPress);
+let task2Words = [];
+const txtTask2Output = document.getElementById("txtTask2Output");
+//TODO: Lage callback-funksjonen txtTask2WordKeyPress
+function txtTask2WordKeyPress(aEvent) {
+  const key = aEvent.key;
+  switch(key) {
+    case "Enter":
+      const words = txtTask2Word.value.split(" ");
+      txtTask2Word.value = "";
+      task2Words = task2Words.concat(words);
+      txtTask2Output.innerHTML = "Number of words: " + task2Words.length + "<br>" + task2Words.join(" ");
+      console.log(task2Words);
+      break;
+  }
+}
+
+//--- Part 3 ----------------------------------------------------------------------------------------------
+/* Put your code below here!*/
+
+const cmbTask3CheckAnswer = document.getElementById("cmbTask3CheckAnswer");
+cmbTask3CheckAnswer.addEventListener("click", cmbTask3CheckAnswerClick);
+const txtTask3Output = document.getElementById("txtTask3Output");
+
+let text = ""; //Tømmer teksten, gjør klar til ny utskrift.
+function cmbTask3CheckAnswerClick() {
+  const chkTask3 = document.getElementsByName("chkTask3");
+  for(let i = 0; i < chkTask3.length; i++) {
+    /*
+    TODO: Sjekk om checkboxen er huket av. og skriv ut resultatet i txtTask3Output
+          Skriv også ut hvilken checkbox som er huket av, og hva verdien er.
+          Det er også mulig å skrivet hele teksten til hver checkbox i txtTask3Output.
+    */
+   const checkBox = chkTask3[i];
+   if(checkBox.checked){
+    const value = checkBox.value;
+    if(value === "4"){
+      text += "Du har valgt nummer " + value + ". Dette kan du ikke mene!!???<br />"
+
+    }else{
+      text += "Du har valgt nummer " + value + ".<br />"
+    }
+   }
+
+  }
+  txtTask3Output.innerHTML = text;
+  text = ""; //Rømmer teksten, klargjør til neste klikk!
+}
+
+//--- Part 4 ----------------------------------------------------------------------------------------------
+/* Put your code below here!*/
+
 const CarTypes = [
   { value: 1, caption: "Aston Martin" },
   { value: 2, caption: "Bentley" },
@@ -16,7 +94,56 @@ const CarTypes = [
   { value: 13, caption: "Nissan" },
 ];
 
+text = " "
+
+
+
+ const keys = Object.keys(CarTypes); 
+
+  for(let index = 0; index < keys.length; index++){
+    
+    const key = keys[index];
+    const { value, caption } = CarTypes[key];
+    const x = document.createElement("divTask4Cars");
+   
+    
+    text +=    caption + newLine
+    document.getElementById("divTask4Cars").innerHTML = " " + text;
+
+
+   
+  }
+  text = " "
+  text += "you have chosen" 
+
+  document.getElementById("txtTask4Output").innerHTML = text
+
+
+
+
+
+
+
+//--- Part 5 ----------------------------------------------------------------------------------------------
+/* Put your code below here!*/
+text = " test "
+
+
+function valueChange5(){
+
+  document.getElementById("txtTask5Output").innerHTML = text
+
+}
+document.getElementById("txtTask5Output").innerHTML = text
+//--- Part 6 ----------------------------------------------------------------------------------------------
+/* Put your code below here!*/
+
 const GirlsNames = ["Anne", "Inger", "Kari", "Marit", "Ingrid", "Liv", "Eva", "Berit", "Astrid", "Bjørg", "Hilde", "Anna", "Solveig", "Marianne", "Randi", "Ida", "Nina", "Maria", "Elisabeth", "Kristin"];
+
+
+
+//--- Part 7 ----------------------------------------------------------------------------------------------
+/* Put your code below here!*/
 
 const MovieGenre = [
   "Action",
@@ -44,24 +171,3 @@ const MovieGenre = [
   "War",
   "Western",
 ];
-
-//--- Part 1 ----------------------------------------------------------------------------------------------
-/* Put your code below here!*/
-
-//--- Part 2 ----------------------------------------------------------------------------------------------
-/* Put your code below here!*/
-
-//--- Part 3 ----------------------------------------------------------------------------------------------
-/* Put your code below here!*/
-
-//--- Part 4 ----------------------------------------------------------------------------------------------
-/* Put your code below here!*/
-
-//--- Part 5 ----------------------------------------------------------------------------------------------
-/* Put your code below here!*/
-
-//--- Part 6 ----------------------------------------------------------------------------------------------
-/* Put your code below here!*/
-
-//--- Part 7 ----------------------------------------------------------------------------------------------
-/* Put your code below here!*/
